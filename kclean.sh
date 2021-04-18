e5499159b117ef1ac690def1cf6d446c5645fcf7
red.f0x <red.f0x@devel0.local>
2021-04-18 15:21:14 +0300

    kclean.sh: purge other kernel's leftovers in /boot like config and System.map

--- kclean.sh
+++ kclean.sh
@@ -7,10 +7,14 @@ mount /boot
 while [ "$1" != "" ];
 do
 	KERNEL=/boot/kernel-$1-$SUFFIX
+	CONFIG=/boot/config-$1-$SUFFIX
+	SMAP=/boot/System.map-$1-$SUFFIX
 	MODULES=/lib/modules/$1-$SUFFIX
 	SOURCES=/usr/src/linux-$1-$SUFFIX
 	emerge -Ca =gentoo-sources-"$1"
 	rm -rf $KERNEL
+	rm -rf $CONFIG
+	rm -rf $SMAP
 	rm -rf $MODULES
 	rm -rf $SOURCES
 	shift
